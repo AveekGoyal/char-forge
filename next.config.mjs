c5daa5193e9482e15034152c1d4426435d6d9d95
Fork Labs <fork.labs.devs@gmail.com>
2024-11-10 04:11:31 +0530

    Eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
